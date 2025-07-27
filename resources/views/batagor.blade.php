3a946310e5a43a22893e652033b8811c3212f8bb
chiaaZee <hinaachaft@gmail.com>
2025-07-28 05:27:31 +0700

    satu

---
+++ resources/views/batagor.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     @vite('resources/css/app.css')
+    <title>Document</title>
+</head>
+<body>
+    <div class="bg-indigo-500">
+        <H1>batagor</H1>
+        <div class="flex flex-col">
+            <button class="hover:cursor-pointer active:cursor-progress focus:cursor-not-allowed">Submit</button>
+        </div>
+    </div>
+    
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Halaman Utama
+Route::get('/', function () {
+    return view('index'); // Pastikan file index.blade.php ada
+})->name('index');
+
+// Halaman About
+Route::get('/about', function () {
+    return view('about'); // Buat file about.blade.php
+})->name('about');
+
+// routes/web.php
+Route::get('/single-post', function () {
+    return view('single-post');
+})->name('single-post');
+
+// Halaman Categories
+Route::get('/category', function () {
+    return view('category'); // File categories.blade.php
+})->name('category');
+
+// Halaman Contact
+Route::get('/contact', function () {
+    return view('contact'); // File contact.blade.php
+})->name('contact');
+
+// Halaman BLog details
+Route::get('/blog-details', function () {
+    return view('blog-details'); // File blog-details.blade.php
+})->name('blog-details');
+
+
